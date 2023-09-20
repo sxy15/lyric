583dc43385f1ac2c73543b4e1409c126b153474c
sxy15 <sxy_19921115@163.com>
2023-09-20 23:07:00 +0800

    feat: 歌词滚动

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <script setup lang="ts">
 import { lrc } from './lrc';
 import audio from './assets/music.mp3';
+import { onMounted, reactive, ref } from 'vue';
+
+let activeIndex = ref(0);
+
+let doms = reactive({
+  audio: null as HTMLAudioElement | null,
+  wrapper: null as HTMLElement | null,
+  lyric: null as HTMLElement | null
+})
+
+let heights = reactive({
+  wrapper: 0,
+  lyric: 0,
+  item: 0
+})
 
 const parseTime = (time: string) => {
   const parts = time.split(':');
   return +parts[0] * 60 + +parts[1];
+}
+
+const findActiveIndex = () => {
+  const currentTime = doms.audio?.currentTime || 0;
+  const index = lrcArr.findIndex(it => it.time > currentTime);
+  return index === -1 ? lrcArr.length - 1 : index - 1;
 }
 
 const lrcArr = lrc.split('\n').map(it => {
@@ -16,6 +37,37 @@ const lrcArr = lrc.split('\n').map(it => {
     words
   }
 })
+
+const setOffset = () => {
+  const offset = heights.item * Math.max(activeIndex.value - 5, 0);
+  const maxOffset = heights.lyric - heights.wrapper;
+  const computedOffset = offset > maxOffset ? maxOffset : offset;
+  (doms.lyric as HTMLElement).style.transform = `translateY(-${computedOffset}px)`
+}
+
+onMounted(() => {
+  const audioDom = document.querySelector('audio') as HTMLAudioElement;
+  const wrapperDom = document.querySelector('.wrapper') as HTMLElement;
+  const lyricDom = document.querySelector('.lyric') as HTMLElement;
+  doms = {
+    audio: audioDom,
+    wrapper: wrapperDom,
+    lyric: lyricDom
+  }
+  const wrapperHeight = doms.wrapper?.clientHeight || 0;
+  const lyricHeight = doms.lyric?.clientHeight || 0;
+  const itemHeight = lyricHeight / lrcArr.length;
+  heights = {
+    wrapper: wrapperHeight,
+    lyric: lyricHeight,
+    item: itemHeight,
+  }
+  doms.audio?.addEventListener('timeupdate', () => {
+    const idx = findActiveIndex()
+    activeIndex.value = idx;
+    setOffset()
+  })
+})
 </script>
 
 <template>
@@ -25,7 +77,8 @@ const lrcArr = lrc.split('\n').map(it => {
     </div>
     <div class="wrapper">
       <div class="lyric">
-        <div class="lyric-li" v-for="item in lrcArr" :key="item.time">{{ item.words }}</div>
+        <div :class="['lyric-li', activeIndex === i && 'active']" v-for="(item, i) in lrcArr" :key="item.time">
+          {{ item.words }}</div>
       </div>
     </div>
   </div>
@@ -56,6 +109,10 @@ audio {
   margin-top: 20px;
 }
 
+.lyric {
+  transition: all 0.2s;
+}
+
 .lyric-li {
   text-align: center;
   list-style: none;
@@ -70,7 +127,7 @@ audio {
   transition: all .2s;
 
   &.active {
-    color: rgb(255, 255, 255);
+    color: rgb(174 81 0);
     transform: scale(1.2);
   }
 }
